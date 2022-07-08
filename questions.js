3fa76276de216e8685682f9acc7f938d07fc82a9
Sasikala Dhanabalan <106913129+sasikalaarunraj@users.noreply.github.com>
2022-07-08 11:10:55 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    question:"Who is your best friend?",
+    options:["A","B","C","D"],
+    answer:0,
+},
+    {
+    question:"Who is your favorite person?",
+    options:["A","B","C","D"],
+     answer:1,
+},
+    {
+    question:"which actor you like more?",
+    options:["A","B","C","D"],
+     answer:2,
+},
+    {
+    question:"what is your name?",
+    options:["A","B","C","D"],
+     answer:3,
+},
+  ]
